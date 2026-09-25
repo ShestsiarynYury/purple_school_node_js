cd61a73b5bf56e59ceea08f2c1cad7647511429a
= <=>
2026-09-25 17:49:58 +0300

    init 7

---
+++ 7-timer-notifications/index.js
@@ -0,0 +1,11 @@
+const notifier = require('node-notifier');
+
+const firstArg = Number.parseInt(process.argv[2]);
+const secondArg = Number.parseInt(process.argv[3]);
+const thirdArg = Number.parseInt(process.argv[4]);
+
+const delay = firstArg * 60 * 60 * 1000 + secondArg * 60 * 1000 + thirdArg * 1000;
+
+setTimeout(() => {
+    notifier.notify('Message');
+}, delay);
